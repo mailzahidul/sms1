65725023b65e42c551cb6ee4d8a2c8e0ac5729af
Zahidul Islam <mailzahidul@gmail.com>
2019-08-01 15:37:34 +0600

    student view update for studen info save

--- sms1/urls.py
+++ sms1/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings  # new
+from django.conf.urls.static import static  # new
 from django.urls import path, include
 from . import views
 
@@ -24,3 +26,6 @@ urlpatterns = [
     path('teachers/', include('teachers.urls')),
     path('', views.home, name="home"),
 ]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- student/views.py
+++ student/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import StudentInfoForm
 
 
 def createStudent(request):
-    return render(request, 'student/createStudent.html')
+    forms = StudentInfoForm()
+    if request.method == 'POST':
+        forms = StudentInfoForm(request.POST, request.FILES)
+        if forms.is_valid():
+            forms.save()
+            return redirect('home')
+    context = {'forms': forms}
+    return render(request, 'student/createStudent.html', context)
