a0ce2b30e0bf8ce7602efe12f9bb7c698fd1bdfb
Zahidul Islam <mailzahidul@gmail.com>
2019-08-05 01:22:50 +0600

    Student view template complete

--- student/urls.py
+++ student/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('list/', views.studentlist, name='student-list'),
     path('class/<class_name>', views.classInfo, name='class-name'),
     path('edit/<student_id>', views.studentEdit, name='student-edit'),
+    path('delete/<student_id>', views.studentDelete, name='student-delete'),
+    path('view/<student_id>', views.studentView, name='student-view'),
 ]
--- student/views.py
+++ student/views.py
@@ -24,10 +24,21 @@ def classInfo(request, class_name):
     return render(request, 'student/classStudents.html', {'list': stu_list, 'class_n': class_name})
 
 
+def studentDelete(request, student_id):
+    std_obj = StudentInfo.objects.get(id=student_id)
+    std_obj.delete()
+    return redirect('student-list')
+
+
+def studentView(request, student_id):
+    std_obj = StudentInfo.objects.get(id=student_id)
+    return render(request, 'student/studentView.html', {'student1': std_obj})
+
+
 def studentEdit(request, student_id):
     std_obj = StudentInfo.objects.get(id=student_id)
     forms = StudentInfoForm(instance=std_obj)
-    print("Request: ",forms)
+    print("Request: ", forms)
     if request.method == 'POST':
         if forms.is_valid():
             forms.save()
